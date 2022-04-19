9040f881ad8529b39da87fb3403434e68125279c
Danny Mulvihill <dmulvi@Dannys-MacBook-Pro.local>
2022-04-19 11:56:44 -0700

    initial commit

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,80 +1,184 @@
 import React, { useState, useEffect } from "react";
-import logo from "./react-logo.svg";
-import nodejsLogo from "./nodejs-logo.svg";
 import "./App.css";
+import axios from "axios";
+import Button from 'react-bootstrap/Button';
+
+function AcquireMap(props) {
+  function getMap() {
+    console.log('get map');
+    axios.get('/api/map')
+      .then(function (response) {
+        // just create a global object for now, save in state later
+        window.goal = response.data.goal;
+      });
+  }
+  return <Button onClick={getMap}>Acquire Megaverse Map</Button>;
+}
+
+function ParseMap(props) {
+  function extractCoordinates() {
+    let coords = [];
+    window.goal.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        if (col === 'POLYANET') {
+          coords.push([rowIndex, colIndex]);
+        }
+      })
+    });
+
+    window.coords = coords;
+  }
+
+  return <Button onClick={extractCoordinates}>Extract Coordinates / Phase 1</Button>;
+}
+
+function CreateMegaverse(props) {
+  function createPolyanets() {
+    window.coords.forEach(coord => {
+      axios.post('/api/create-polyanet', {candidateId: 'd5bae61f-59d3-40ca-98ef-5667082a3710', row: coord[0], column: coord[1]})
+      .then(function (response) {
+        console.log('created polyanet at coords: ', coord);
+      });
+    });
+
+  }
+  return <Button onClick={createPolyanets}>Add Polyanets</Button>;
+}
+
+function ParseMapPhase2(props) {
+  function extractCoordinates() {
+    let coords = {
+      POLY: [],
+      SOL: [],
+      ETH: []
+    }
+    var text;
+    window.goal.forEach((row, rowIndex) => {
+      row.forEach((col, colIndex) => {
+        if (col === 'POLYANET') {
+          coords.POLY.push([rowIndex, colIndex]);
+        }
+        else if (col.indexOf('SOLOON') !== -1) {
+          text = col.split("_");
+          coords.SOL.push([rowIndex, colIndex, text[0].toLowerCase()]);
+        }
+        else if (col.indexOf('COMETH') !== -1) {
+          text = col.split("_");
+          coords.ETH.push([rowIndex, colIndex, text[0].toLowerCase()]);
+        }
+      })
+    });
+
+    window.coords = coords;
+  }
+
+  return <Button onClick={extractCoordinates}>Extract Coordinates / Phase 2</Button>;
+}
+
+/* create object utility functions
+   initially, I simply made all the api calls in a foreach loop, however with the second challenge 
+   the number of API calls seemed to overwhelm the endpoint, resulting in random ? question marks
+   I switched to this recursive approach that only makes the next call after the promise completes
+   to fix that issue.
+*/
+window.polyIndex = 0;
+function createPoly() {
+  axios.post('/api/create-polyanet', {
+    candidateId: 'd5bae61f-59d3-40ca-98ef-5667082a3710', 
+    row: window.coords.POLY[window.polyIndex][0], 
+    column: window.coords.POLY[window.polyIndex][1]
+  })
+  .then(function (response) {
+    if (window.coords.POLY.length > (++window.polyIndex)) {
+      console.log('recursively creating planets');
+      createPoly();
+    }
+  });
+}
+
+window.solIndex = 0;
+function createSol() {
+  axios.post('/api/create-soloon', {
+    candidateId: 'd5bae61f-59d3-40ca-98ef-5667082a3710', 
+    row: window.coords.SOL[window.solIndex][0], 
+    column: window.coords.SOL[window.solIndex][1],
+    color: window.coords.SOL[window.solIndex][2],
+  })
+  .then(function (response) {
+    //console.log('created SOLanet at coords: ', coord);
+    if (window.coords.SOL.length > (++window.solIndex)) {
+      console.log('recursively creating soloon');
+      createSol();
+    }
+  });
+}
+
+window.ethIndex = 0;
+function createEth() {
+  axios.post('/api/create-cometh', {
+    candidateId: 'd5bae61f-59d3-40ca-98ef-5667082a3710', 
+    row: window.coords.ETH[window.ethIndex][0], 
+    column: window.coords.ETH[window.ethIndex][1],
+    direction: window.coords.ETH[window.ethIndex][2],
+  })
+  .then(function (response) {
+    //console.log('created SOLanet at coords: ', coord);
+    if (window.coords.ETH.length > (++window.ethIndex)) {
+      console.log('recursively creating cometh');
+      createEth();
+    }
+  });
+}
+
+function CreateMegaversePhase2(props) {
+  function createCrossmintVerse() {
+    // use recursive function instead of forEach loop
+    createPoly();
+    createSol();
+    createEth();
+
+    //POLYANETS
+    // window.coords.POLY.forEach(coord => {
+    //   axios.post('/api/create-polyanet', {candidateId: 'd5bae61f-59d3-40ca-98ef-5667082a3710', row: coord[0], column: coord[1]})
+    //   .then(function (response) {
+    //     console.log('created polyanet at coords: ', coord);
+    //     //console.log(response.data);
+    //   });
+    // });
+    //SOLOONS
+    // window.coords.SOL.forEach(coord => {
+    //   axios.post('/api/create-soloon', {candidateId: 'd5bae61f-59d3-40ca-98ef-5667082a3710', row: coord[0], column: coord[1], color: coord[2]})
+    //   .then(function (response) {
+    //     console.log('created soloon at coords: ', coord);
+    //     //console.log(response.data);
+    //   });
+    // });
+    // //COMETHS
+    // window.coords.ETH.forEach(coord => {
+    //   axios.post('/api/create-cometh', {candidateId: 'd5bae61f-59d3-40ca-98ef-5667082a3710', row: coord[0], column: coord[1], direction: coord[2]})
+    //   .then(function (response) {
+    //     console.log('created cometh at coords: ', coord);
+    //     //console.log(response.data);
+    //   });
+    // });
+  }
+  return <Button onClick={createCrossmintVerse}>Create Crossmint Megaverse</Button>;
+}
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/users")
-      .then(res => res.json())
-      .then(json => setUsers(json.users));
-    // Specify how to clean up after this effect:
-    return () => {};
-  }, []); // empty 2nd arg - only runs once
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Node Template</h1>
-        <p>
-          <a
-            className="App-link"
-            href="https://github.com/mattvukas/react-node-template"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://github.com/mattvukas/react-node-template
-          </a>
-        </p>
-        <div className="logo-box">
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={nodejsLogo} className="Node-logo" alt="nodejsLogo" />
-        </div>
-        <p>
-          Edit <code>client/src/App.js</code> and save to reload React app.
-        </p>
-        <p>
-          Edit <code>client/server/routes/api.js</code> and save to reload
-          Node.js app.
-        </p>
-        <br />
-        <p>
-          <code>GET /api/users</code>:{" "}
-          {users.length ? users.join(", ") : "loading..."}
-        </p>
-        <br />
-        <p>
-          Docs:{" "}
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://nodejs.org/en/docs/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Node.js
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://expressjs.com/en/4x/api.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Express.js
-          </a>
-        </p>
-      </header>
+    <div className="App" >
+      <div className="content">
+      <div className="d-grid gap-2">
+      <AcquireMap/>
+      <h3>Phase 1</h3>
+      <ParseMap/>
+      <CreateMegaverse />
+      <h3>Phase 2</h3>
+      <ParseMapPhase2/>
+      <CreateMegaversePhase2 />
+      </div>
+      </div>
     </div>
   );
 }
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,10 +1,38 @@
 const express = require("express");
 const router = express.Router();
+const axios = require('axios').default;
 
-/* GET users listing. */
-router.get("/users", function(req, res, next) {
-  console.log('get "users" route hit');
-  res.send({ users: ["joe", "bernie", "tulsi", "donald", "bill"] });
+
+/* GET goal map */
+router.get("/map", function(req, res, next) {
+  axios.get('https://challenge.crossmint.io/api/map/d5bae61f-59d3-40ca-98ef-5667082a3710/goal')
+  .then(function (response) {
+    res.send(response.data);
+  });
+});
+
+/* call the polyanets endpoint */
+router.post("/create-polyanet", function(req, res, next) {
+  axios.post('https://challenge.crossmint.io/api/polyanets', req.body)
+  .then(function (response) {
+    res.send(response.data);
+  });
+});
+
+/* call the soloons endpoint */
+router.post("/create-soloon", function(req, res, next) {
+  axios.post('https://challenge.crossmint.io/api/soloons', req.body)
+  .then(function (response) {
+    res.send(response.data);
+  });
+});
+
+/* call the comeths endpoint */
+router.post("/create-cometh", function(req, res, next) {
+  axios.post('https://challenge.crossmint.io/api/comeths', req.body)
+  .then(function (response) {
+    res.send(response.data);
+  });
 });
 
 module.exports = router;
